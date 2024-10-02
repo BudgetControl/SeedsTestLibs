8b4a9d19aef863397fbad3f2e17e5d800f03341f
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2024-10-02 16:15:24 +0200

    added budget configuration object on seeders

--- src/Resources/Seeds/BudgetSeed.php
+++ src/Resources/Seeds/BudgetSeed.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Budgetcontrol\Seeds\Resources\Seeds;
 
 use Budgetcontrol\Library\Model\Budget;
-use Illuminate\Database\Seeder;
+USE Budgetcontrol\Library\ValueObject\BudgetConfiguration;
 
 class BudgetSeed extends Seed
 {
@@ -13,13 +13,23 @@ class BudgetSeed extends Seed
      */
     public static function run(): void
     {
+        $configuration = BudgetConfiguration::create(
+            [],
+            ['incoming', 'expenses', 'debit'],
+            'recursively',
+            [196],
+            [],
+            '3333-11-19T02:08:00.000000Z',
+            '2024-08-19T00:00:00.000000Z'
+        );
+        
         Budget::create([
             'uuid' => \Ramsey\Uuid\Uuid::uuid4()->toString(),
             'name' => 'Budget 1',
             'description' => 'Budget 1 description',
             'amount' => 1000,
             'workspace_id' => 1,
-            'configuration' => '{"tags": [], "types": ["incoming", "expenses", "debit"], "period": "recursively", "accounts": [196], "end_date": "3333-11-19T02:08:00.000000Z", "categories": [], "period_end": "2024-08-19T00:00:00.000000Z", "start_date": "2024-06-08T00:00:00.000000Z", "period_start": "2024-07-17T00:00:00.000000Z"}',
+            'configuration' => $configuration,
             'notification' => 1,
             'emails' => ['foo@email.it', 'bar@email.it']
         ]);
